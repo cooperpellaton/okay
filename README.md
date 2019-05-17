2e262d3c26ffca37387948414227b06ac89c2b68
Cooper Pellaton <cooper@cooperpellaton.com>
2019-05-17 00:36:10 -0400

    [add] more GIFs and a README

---
+++ README.md
@@ -0,0 +1,17 @@
+# OKAY
+
+I like [Pete Davidson](https://en.wikipedia.org/wiki/Pete_Davidson)'s portrayal of the "[dumb Chad](https://www.youtube.com/watch?v=fF6gExZu-2M)" on SNL, and I frequently use the "okay" response.
+
+This is a [Discord](https://discordapp.com/) bot that will serve up Pete Davidson gifs!
+
+## Commands
+
+* `?okay`
+  * Returns an "okay" gif.
+* `?notokay`
+  * Returns a random Pete Davidson gif that is **not** okay.
+
+## Use This
+
+[Add](https://discordapp.com/api/oauth2/authorize?client_id=578782942864605205&permissions=22592&scope=bot) me to your server!
+
--- bot.py
+++ bot.py
@@ -1,22 +1,29 @@
-import discord
-import os
-from discord.ext import commands
-import random
-
-description = """I like Pete Davidson, you like Pete Davidson so I made a bot, okay?
+"""I like Pete Davidson, you like Pete Davidson so I made a bot, okay?
 Okay.
 """
+
+import os
+import random
+
+import discord
+from discord.ext import commands
 
 bot = commands.Bot(command_prefix="?", description=description)
 
 okays = [
     "https://media.giphy.com/media/xT9IgrlyzJlE6ljtS0/giphy.gif",
+    "https://thumbs.gfycat.com/ForcefulHopefulJapanesebeetle-max-1mb.gif"
     "https://tenor.com/MWVF.gif",
     "https://tenor.com/TKX1.gif",
     "https://tenor.com/7geU.gif",
     "https://tenor.com/GS73.gif",
 ]
 
+others = [
+    "https://media.giphy.com/media/3o7aDaYBtNbdNv1eWQ/giphy.gif",
+    "https://thumbs.gfycat.com/UltimateGorgeousIberianmole-max-1mb.gif",
+    "https://media.giphy.com/media/3o7TKu6WJxtZvbqKv6/giphy.gif",
+]
 
 @bot.event
 async def on_ready():
@@ -29,6 +36,12 @@ async def okay(ctx):
     await ctx.send(random.choice(okays))
 
 
+@bot.command(description='Returns a random gif of Pete Davidson.')
+async def notokay(ctx):
+    print("not okay")
+    await ctx.send(random.choice(others))
+
+
 # DISCORDTOKEN ENV VAR MUST BE SET OR SERVER WILL NOT RUN
-# IE EXPORT DISCORDTOKEN=XXXXXXXXXXXXXXX
+# EXPORT DISCORDTOKEN=XXXXXXXXXXXXXXX
 bot.run(os.environ.get("DISCORDTOKEN"))
