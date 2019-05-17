ea5d782b202531510f0153b8bcdc2e8524bd2da7
Cooper Pellaton <cooper@cooperpellaton.com>
2019-05-17 00:47:54 -0400

    [add] more gifs

--- bot.py
+++ bot.py
@@ -21,11 +21,30 @@ okays = [
     "https://tenor.com/GS73.gif",
 ]
 
-others = [
+morepete = [
     "https://media.giphy.com/media/3o7aDaYBtNbdNv1eWQ/giphy.gif",
     "https://thumbs.gfycat.com/UltimateGorgeousIberianmole-max-1mb.gif",
     "https://media.giphy.com/media/3o7TKu6WJxtZvbqKv6/giphy.gif",
+    "https://media0.giphy.com/media/xUOwG0QGuvAbBcOhyM/source.gif",
+    "https://media2.giphy.com/media/8jBExrz7Axgha/giphy.gif",
+    "https://media0.giphy.com/media/1xmXet0VtamgfrFLbd/source.gif",
+    "https://media2.giphy.com/media/NQUuOKTR6aHCmbwBRm/source.gif",
+    "https://media1.giphy.com/media/djVuHoknhk89y/source.gif",
+    "https://media2.giphy.com/media/3j1cVdG8uWR82e8OJG/source.gif",
+    "https://media2.giphy.com/media/9S1yaD4TfqLueDAtRL/source.gif",
+    "https://media.tenor.com/images/020e9694a25eed2b90b881678e263981/tenor.gif",
 ]
+
+notpete = [
+    "https://media1.giphy.com/media/l0NgRiyoFdg6uspoc/source.gif",
+    "https://media.giphy.com/media/Bo1lBI2y1vT0mQ0Ttv/giphy.gif",
+    "https://media.giphy.com/media/3o6Mb7fSXeAWwGV7Hy/giphy.gif",
+    "https://media.giphy.com/media/3o7TKy3KWDYOA7OUSI/giphy.gif",
+    "https://media1.tenor.com/images/9950127c0d9d4be1f6f56ee59ecf99e1/tenor.gif?itemid=7545767",
+    "http://gif-finder.com/wp-content/uploads/2016/01/Maya-Rudolph-Okay.gif",
+    "https://tenor.com/view/okay-question-confused-gif-11384829",
+]
+
 
 @bot.event
 async def on_ready():
@@ -38,10 +57,22 @@ async def okay(ctx):
     await ctx.send(random.choice(okays))
 
 
-@bot.command(description='Returns a random gif of Pete Davidson.')
+@bot.command(description="Returns a random gif of Pete Davidson.")
 async def notokay(ctx):
     print("not okay")
-    await ctx.send(random.choice(others))
+    await ctx.send(random.choice(morepete))
+
+
+@bot.command(description="Returns a random gif that does NOT contain Pete Davidson.")
+async def notpete(ctx):
+    print("not Pete")
+    await ctx.send(random.choice(notpete))
+
+
+@bot.command(description="Returns a gif explaining you're a dick.")
+async def dick(ctx):
+    print("Someone was a dick.")
+    await ctx.send("https://media1.giphy.com/media/7JgyqZYNM4T3T59Q3H/source.gif")
 
 
 # DISCORDTOKEN ENV VAR MUST BE SET OR SERVER WILL NOT RUN
